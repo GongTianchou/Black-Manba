2d3f9d6ae90d62723eea0d2db4d5fc1bf78357c6
Tianchou Gong <tianchougong@gmail.com>
2022-03-23 14:13:12 -0400

    project

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# Black-Manba
+# Black Mamba
+
+![screenshot](assets/images/screenshot2.png)
+
+## Deployed application:
+https://ellysecarter.github.io/black-mamba/
+
+## User Story
+
+```
+AS A NBA fan
+I WANT an app that can search players' stats and news stories,
+SO THAT so that I can keep up with the latest NBA news.
+```
+
+## Table of Contents
+* [Technologies used](#technologies-used)
+* [Features](#features)
+* [Known bugs](#known-bugs)
+* [Images](#stock-images-provided-by)
+* [Contributors](#contributors)
+
+
+## Technologies Used
+* HTML
+* CSS
+* JavaScript
+* Bootstrap
+* Foundation
+* [API-NBA](https://api-sports.io/documentation/nba/v2)
+* [GNews](https://gnews.io/)
+
+
+## Features
+* Start by typing in a NBA Player's name.
+* It will then populate that player's stats
+* It will also provide news stories associated with that player
+
+## Known Bugs
+* API-NBA and GNews both have a daily quota of 100 API calls. If you search for a player and it does not populate a result, it is because we have hit our daily limit of 100 requests. 
+
+
+## Stock images provided by:
+* [Mike Von](https://unsplash.com/photos/Q-3AcRtqoqM)
+
+
+
+## Contributors
+* Ellyse Carter [@ellysecarter](https://github.com/ellysecarter)
+* Tianchou Gong [@GongTianchou](https://github.com/GongTianchou) 
+* Vishal Mangla [@vishalmangla](https://github.com/vishalmangla) 
---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+var playerContainer = document.getElementById("results-container")
+var inputEl = document.getElementById("username");
+var submitbtn = document.getElementById('btn')
+var userArray = [];
+
+var formSubmitHandler = function (event){
+    event.preventDefault();
+    
+    var playerName = inputEl.value.trim();
+
+    if (username){
+        playerContainer.textContent = "";
+        inputEl.value = "";
+    }
+    
+    fetch('https://gnews.io/api/v4/search?q='+playerName+'&token=761d21cd3ee94c2d0b71ba65f6b3f22d')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        var maxlength = 5;
+        if(data.articles.length < 5){
+            maxlength = data.articles.length;
+        }
+        
+        for (var i = 0; i < maxlength; i++){
+        var newsName = document.createElement('h3');
+        var newsUrl = document.createElement('p')
+
+        newsName.textContent = data.articles[i].title;
+        newsUrl.textContent = data.articles[i].url;
+
+        playerContainer.append(newsName);
+        playerContainer.append(newsUrl);
+    
+    }
+    });
+    
+    var user = inputEl.value.trim();
+    userArray.push(user);
+    localStorage.setItem("user",JSON.stringify(userArray));
+}
+submitbtn.addEventListener('click',formSubmitHandler);
+
+var recentSearches = JSON.parse(localStorage.getItem("user"))
+var showSearch = document.getElementById("search-list")
+var clearBtn = document.getElementById("clear");
+
+function displaySearches() {
+    for (var i = 0; i < recentSearches.length; i++) {
+        var entries = document.createElement("li");
+        entries.textContent = recentSearches[i] + " ---- ";
+
+        entries.setAttribute("style", "margin:auto; width:20%; ");
+        showSearch.appendChild(entries);
+    }
+
+}
+
+// clear recent search history from LS and remove from page test test
+clearBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    localStorage.clear("user", JSON.stringify(userArray));
+    document.getElementById("search-list").style.display = "none";
+    document.getElementById("clear").style.display = "none";
+    })
+
+displaySearches();
